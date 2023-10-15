4efd3dd3e75215a221cb8574c33e77de7c745a9e
Webdeveloperintraining <sil22006@byui.edu>
2023-10-14 23:53:03 -0700

    page css styling mobile view improved

--- js/mealAPIdata.js
+++ js/mealAPIdata.js
@@ -1,1 +1,15 @@
-const url = "www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
+async function fetchData() {
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const data = await response.json();
+  // Do something with the data
+  console.log(data);
+}
+
+fetchData();
+
+const urlIngredients ='https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+
+const urlRandomMeals='https://www.themealdb.com/api/json/v1/1/random.php';
